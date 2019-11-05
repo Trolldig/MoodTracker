d0c880fb7786bc78fc588b54774b8c137ce8bc7b
Dong <mklipfel@gmx.de>
2019-11-05 22:54:00 +0100

    Initial commit

---
+++ app/src/main/java/de/matthias/klipfel/moodtracker/MainActivity.java
@@ -0,0 +1,33 @@
+package de.matthias.klipfel.moodtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.voteButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openVoteActivity();
+            }
+        });
+    }
+
+    public void openVoteActivity(){
+        Intent intent = new Intent(this, VoteActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/de/matthias/klipfel/moodtracker/VoteActivity.java
@@ -0,0 +1,14 @@
+package de.matthias.klipfel.moodtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vote);
+    }
+}
