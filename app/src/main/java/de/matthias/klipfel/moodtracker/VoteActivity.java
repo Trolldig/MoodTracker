b445daa78bf47fd3c732146d0f5151e3fdaee194
Dong <mklipfel@gmx.de>
2020-01-05 23:27:33 +0100

    Config first vote
    Change the names of the first vote
    Add Listener for the rating to get the selected Level

--- app/src/main/java/de/matthias/klipfel/moodtracker/VoteActivity.java
+++ app/src/main/java/de/matthias/klipfel/moodtracker/VoteActivity.java
@@ -3,13 +3,61 @@ package de.matthias.klipfel.moodtracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.hsalf.smilerating.BaseRating;
+import com.hsalf.smilerating.SmileRating;
 
 public class VoteActivity extends AppCompatActivity {
+
+    private static final String TAG = "VoteActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote);
+
+        //Get Id of the first positive Vote
+        SmileRating posAff1 = (SmileRating) findViewById(R.id.smile_rating);
+        //Change name of smileys
+        posAff1.setNameForSmile(BaseRating.TERRIBLE, "Gar nicht");
+        posAff1.setNameForSmile(BaseRating.BAD, "Wenig");
+        posAff1.setNameForSmile(BaseRating.OKAY, "Einigermaßen");
+        posAff1.setNameForSmile(BaseRating.GOOD, "Erheblich");
+        posAff1.setNameForSmile(BaseRating.GREAT, "Äußerst");
+        //Set Listener for the first Vote
+        posAff1.setOnSmileySelectionListener(new SmileRating.OnSmileySelectionListener() {
+            @Override
+            public void onSmileySelected( int smiley, boolean reselected){
+                // reselected is false when user selects different smiley that previously selected one
+                // true when the same smiley is selected.
+                // Except if it first time, then the value will be false.
+                switch (smiley) {
+                    case SmileRating.BAD:
+                        Log.i(TAG, "Wenig");
+                        break;
+                    case SmileRating.GOOD:
+                        Log.i(TAG, "Good");
+                        break;
+                    case SmileRating.GREAT:
+                        Log.i(TAG, "Great");
+                        break;
+                    case SmileRating.OKAY:
+                        Log.i(TAG, "Okay");
+                        break;
+                    case SmileRating.TERRIBLE:
+                        Log.i(TAG, "Terrible");
+                        break;
+                }
+            }
+        });
+        //Get selected Smiley for the first Vote
+        posAff1.setOnRatingSelectedListener(new SmileRating.OnRatingSelectedListener() {
+            @Override
+            public void onRatingSelected(int level, boolean reselected) {
+                Log.i(TAG, "Level: " + level);
+            }
+        });
     }
 }
