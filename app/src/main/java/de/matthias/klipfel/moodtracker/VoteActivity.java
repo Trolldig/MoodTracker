760a239c0018825f4214778ba7b5d2e11b4b998d
Dong <mklipfel@gmx.de>
2020-01-05 22:10:47 +0100

    Change Manifest
    Delte Back-Button-Code in the VoteActivity and update the manifest instead

--- app/src/main/java/de/matthias/klipfel/moodtracker/VoteActivity.java
+++ app/src/main/java/de/matthias/klipfel/moodtracker/VoteActivity.java
@@ -11,17 +11,5 @@ public class VoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case android.R.id.home:
-                finish();
-                break;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
